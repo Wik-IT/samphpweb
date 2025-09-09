3728286558d64fe9c3dee22b1c78b3e28f40616b
Dan Oancea <dan.oancea@wikit.ro>
2025-09-09 13:21:35 +0300

    update

---
+++ config/dbconfig.xml.php
@@ -0,0 +1,13 @@
+<?php/* 
+<CONFIG application="SAM" version="3.4.2">
+	<Database>
+		<Driver>MYSQL</Driver>
+		<Host>localhost</Host>
+		<Port>3306</Port>
+		<Database>SAMDB</Database>
+		<Username>user</Username>
+		<Password>password</Password>
+	</Database>
+</CONFIG>
+
+ */?>
